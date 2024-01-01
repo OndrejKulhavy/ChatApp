010a44b0ccac32c7e55de266fcdca14be7387fd4
Ondřej Kulhavý <okulhav@gmail.com>
2024-01-01 21:12:23 +0100

    feat: auth logic

--- app/app.py
+++ app/app.py
@@ -1,46 +1,25 @@
+import os
+from flask import Flask
+from flask.cli import load_dotenv
 from flask_socketio import SocketIO
-from flask import Flask, Blueprint, render_template
-from flask_login import LoginManager
+from routing import routing
+from auth import auth
+import pymysql.cursors
 
 app = Flask(__name__)
+
+# Configuration
 app.debug = True
+load_dotenv()
+app.secret_key = os.getenv("SECRET_KEY")
+
+# SocketIO setup
 socketio = SocketIO(app)
 
-# Initialize LoginManager
-
-# Create a blueprint
-main = Blueprint('main', __name__)
-auth = Blueprint('auth', __name__)
-
-
-@main.route('/')
-def index():
-    return render_template('login.html')
-
-
-@main.route('/chat')
-def chat():
-    return render_template('app.html')
-
-
-@auth.route('/login')
-def login():
-    pass
-
-
-@auth.route('/signup')
-def register():
-    pass
-
-
-@auth.route('/logout')
-def logout():
-    pass
-
-
-# Register the blueprint with the app
+# Blueprint registration
 app.register_blueprint(auth)
-app.register_blueprint(main)
+app.register_blueprint(routing)
 
 if __name__ == '__main__':
+    # Run the application using SocketIO
     socketio.run(app, allow_unsafe_werkzeug=True)
